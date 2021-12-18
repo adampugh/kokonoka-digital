626a6cc59d226306aa57bdf6ff8e12908d230746
Adam <adampugh@zoho.com>
2021-12-18 21:19:24 +0000

    add project page

--- src/Project1/index.jsx
+++ src/Project1/index.jsx
@@ -1,8 +1,16 @@
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import './Project1.scss'
 
 import Nav from '../Nav';
 import Contact from '../Contact';
 import Footer from '../Footer';
+import Loading from '../Loading';
+
+import Img from '../assets/img/345.webp'
+import Img2 from '../assets/img/104.webp'
+import Img3 from '../assets/img/84.webp'
+import Img4 from '../assets/img/94.webp'
 
 const containerVariants = {
     exit: {
@@ -14,13 +22,54 @@ const containerVariants = {
     }
 }
 
+const childVariants = {
+    hidden: {
+        opacity: 0,
+        y: 100,
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+            delay: 1.5
+        }
+    },
+};
 const Project1 = () => {
-    return (
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+    
+    return loading ? (<Loading />) : (
         <motion.div variants={containerVariants} exit="exit">
             <Nav />
-            <div className="container">
-                <h1>Project 1</h1>
-            </div>
+            <motion.div className="container" variants={childVariants} initial="hidden" animate="visible">
+                <div id="project">
+                    <div className="project__text">
+                        <h1>Working with colours to create a brand new exciting experience for an excellent energy brand. Utilizing our excellence, new moves and continuing to move forward with new ideas.</h1>
+                    </div>
+                    <div className="project__grid">
+                        <div className="project__grid__img">
+                            <img src={Img} alt="" />
+                        </div>
+                        <div className="project__grid__img">
+                            <img src={Img2} alt="" />
+                        </div>
+                        <div className="project__grid__img">
+                            <img src={Img3} alt="" />
+                        </div>
+                        <div className="project__grid__img">
+                            <img src={Img4} alt="" />
+                        </div>
+                    </div>
+                
+                </div>
+            </motion.div>
             <Contact />
             <Footer />
         </motion.div>
