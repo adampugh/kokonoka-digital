cc27915367b91bd04da055db0a7215c08098fec2
Adam <adampugh@zoho.com>
2022-04-02 21:35:28 +0100

    cleanup

--- src/Brands/index.jsx
+++ src/Brands/index.jsx
@@ -1,12 +1,11 @@
 import './Brands.scss';
-import { motion, useAnimation  } from 'framer-motion';
-import {useInView} from "react-intersection-observer";
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import HP from '../assets/brands/hp.png';
 import Chanel from '../assets/brands/chanel.png';
 import PF from '../assets/brands/pf.png';
 import Plaid from '../assets/brands/plaid.png';
-
 
 const childVariants = {
     hidden: {
@@ -17,13 +16,13 @@ const childVariants = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: 0.6
-        }
+            duration: 0.6,
+        },
     },
 };
 
 const Brands = () => {
-    const {inView, ref} = useInView();
+    const { inView, ref } = useInView();
     const animationControl = useAnimation();
     if (inView) {
         animationControl.start({
@@ -34,40 +33,39 @@ const Brands = () => {
                 when: 'beforeChildren',
                 staggerChildren: 0.4,
                 delay: 0.5,
-                duration: 1
+                duration: 1,
             },
         });
     }
-    
+
     return (
-        <div id="brands" ref={ref}>
-        
-        <motion.div className="container" 
-            initial={{opacity: 0,y: 200}} 
-            animate={animationControl}>
-                <div className="brands__grid">
-                    <motion.div className="brand__logo" variants={childVariants} initial="hidden" animate="visible">
-                        <img src={HP} alt="brand" />
+        <div id='brands' ref={ref}>
+            <motion.div className='container' initial={{ opacity: 0, y: 200 }} animate={animationControl}>
+                <div className='brands__grid'>
+                    <motion.div className='brand__logo' variants={childVariants} initial='hidden' animate='visible'>
+                        <img src={HP} alt='brand' />
                     </motion.div>
-                    <motion.div className="brand__logo" variants={childVariants} initial="hidden" animate="visible">
-                        <img src={Chanel} alt="brand" />
+                    <motion.div className='brand__logo' variants={childVariants} initial='hidden' animate='visible'>
+                        <img src={Chanel} alt='brand' />
                     </motion.div>
-                    <motion.div className="brand__logo" variants={childVariants} initial="hidden" animate="visible">
-                        <img src={PF} alt="brand" />
+                    <motion.div className='brand__logo' variants={childVariants} initial='hidden' animate='visible'>
+                        <img src={PF} alt='brand' />
                     </motion.div>
-                    <motion.div className="brand__logo" variants={childVariants} initial="hidden" animate="visible">
-                        <img src={Plaid} alt="brand" />
+                    <motion.div className='brand__logo' variants={childVariants} initial='hidden' animate='visible'>
+                        <img src={Plaid} alt='brand' />
                     </motion.div>
                 </div>
-                <motion.div variants={childVariants}  initial="hidden" animate="visible">
-                <hr />
-                <p>"Kokonoka are fantastic and I would use them again and again. Excellent work and fantastic turnaround."</p>
-                <div className="brands__quote">David Moore, CEO Princo</div>
+                <motion.div variants={childVariants} initial='hidden' animate='visible'>
+                    <hr />
+                    <p>
+                        "Kokonoka are fantastic and I would use them again and again. Excellent work and fantastic
+                        turnaround."
+                    </p>
+                    <div className='brands__quote'>David Moore, CEO Princo</div>
                 </motion.div>
             </motion.div>
-            
         </div>
-    )
-}
+    );
+};
 
 export default Brands;
--- src/Work/index.jsx
+++ src/Work/index.jsx
@@ -3,53 +3,51 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
 
-const Work = () => {
-    return (
-        <div id='work'>
-            <h2 className='title-spaced'>
-                The Latest <FontAwesomeIcon icon={faNewspaper} />
-            </h2>
-            <div className='work__grid'>
-                <Link to='/45N'>
-                    <div id='work1' className='work__grid__box work__grid__box--white'>
-                        <div className='work__grid__box__overlay'>
-                            <span className='work__grid__box__button'>Work</span>
-                            <h3>Introducing the Truth & Beauty Index: Unlock the secrets of digital transformation</h3>
-                        </div>
+const Work = () => (
+    <div id='work'>
+        <h2 className='title-spaced'>
+            The Latest <FontAwesomeIcon icon={faNewspaper} />
+        </h2>
+        <div className='work__grid'>
+            <Link to='/45N'>
+                <div id='work1' className='work__grid__box work__grid__box--white'>
+                    <div className='work__grid__box__overlay'>
+                        <span className='work__grid__box__button'>Work</span>
+                        <h3>Introducing the Truth & Beauty Index: Unlock the secrets of digital transformation</h3>
                     </div>
-                </Link>
-                <Link to='/homeKitchen'>
-                    <div id='work2' className='work__grid__box work__grid__box--black'>
-                        <div className='work__grid__box__overlay'>
-                            <span className='work__grid__box__button'>Work</span>
-                            <h3>Clean minimalist design for meal-prep subscription service: HomeKitchen</h3>
-                        </div>
+                </div>
+            </Link>
+            <Link to='/homeKitchen'>
+                <div id='work2' className='work__grid__box work__grid__box--black'>
+                    <div className='work__grid__box__overlay'>
+                        <span className='work__grid__box__button'>Work</span>
+                        <h3>Clean minimalist design for meal-prep subscription service: HomeKitchen</h3>
                     </div>
-                </Link>
-                <Link to='/kamura'>
-                    <div id='work3' className='work__grid__box work__grid__box--black'>
-                        <div className='work__grid__box__overlay'>
-                            <span className='work__grid__box__button'>Work</span>
-                            <h3>Introducing the beauty of the new thing that you need.</h3>
-                        </div>
+                </div>
+            </Link>
+            <Link to='/kamura'>
+                <div id='work3' className='work__grid__box work__grid__box--black'>
+                    <div className='work__grid__box__overlay'>
+                        <span className='work__grid__box__button'>Work</span>
+                        <h3>Introducing the beauty of the new thing that you need.</h3>
                     </div>
-                </Link>
-                <Link to='/allied-energy'>
-                    <div id='work4' className='work__grid__box work__grid__box--white'>
-                        <div className='work__grid__box__overlay'>
-                            <span className='work__grid__box__button'>Work</span>
-                            <h3>Health & Growth - Working with Medican to create sleek and polished branding</h3>
-                        </div>
+                </div>
+            </Link>
+            <Link to='/allied-energy'>
+                <div id='work4' className='work__grid__box work__grid__box--white'>
+                    <div className='work__grid__box__overlay'>
+                        <span className='work__grid__box__button'>Work</span>
+                        <h3>Health & Growth - Working with Medican to create sleek and polished branding</h3>
                     </div>
-                </Link>
-            </div>
-            <div className='work__button__wrapper'>
-                <Link to='/work'>
-                    <button className='fade-button'>View all work</button>
-                </Link>
-            </div>
+                </div>
+            </Link>
         </div>
-    );
-};
+        <div className='work__button__wrapper'>
+            <Link to='/work'>
+                <button className='fade-button'>View all work</button>
+            </Link>
+        </div>
+    </div>
+);
 
 export default Work;
